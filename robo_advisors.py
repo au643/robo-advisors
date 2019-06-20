7374431762c920e6cc11cb20682d92a62074ca53
Asli Uyanik <au643@stern.nyu.edu>
2019-06-20 19:09:54 -0400

    dynamic latest close

--- robo_advisors.py
+++ robo_advisors.py
@@ -1,7 +1,7 @@
 # app/robo_advisor.py
 import requests
 import json 
-
+import datetime
 request_url = "http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
@@ -10,17 +10,26 @@ response = requests.get(request_url)
 #print(response.text)
 #
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 parsed_response = json.loads(response.text)
+tsd = parsed_response ["Time Series (Daily)"]
+dates = list(tsd.keys())
+latest_day = dates [0]
+
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
+now = datetime.datetime.now()
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE:  {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
